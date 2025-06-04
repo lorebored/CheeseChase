ad90345b9126fbee7f31c98fe2b346b748961ab0
lorebored <108958186+lorebored@users.noreply.github.com>
2025-06-04 08:45:42 +0200

    Add files via upload
    Versione finale del gioco

---
+++ CheeseChase_V0.0.1/formEnd.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheeseChase_V0._0._1
+{
+    public partial class formEnd : Form
+    {
+        string soldiE;
+        string secondiE;
+        public formEnd(int secondi, int soldi)
+        {
+            InitializeComponent();
+            soldiE = soldi.ToString();
+            secondiE = secondi.ToString();
+
+            lblSoldi.Text = soldiE;
+            lblSecondi.Text = secondiE;
+        }
+
+        private void s(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
